e1e34c2457faebb7c65798356a963b717a5abd94
luisjoia <luisjoia93@gmaikl.com>
2022-05-05 09:16:35 +0100

    implementation of the method getUppercase done

--- src/main/java/TextAnalyser.java
+++ src/main/java/TextAnalyser.java
@@ -9,11 +9,26 @@ public class TextAnalyser {
     }
 
     public String getUppercase(String text, int nthChar) {
+        String uppercaseLetters = "";
+
+        if(text.length() < nthChar || nthChar <= 0) return uppercaseLetters;
+
+        for(int i = nthChar ; i <= text.length(); i+=nthChar){
+
+            if (Character.isUpperCase(text.charAt(i - 1))){
+
+                uppercaseLetters += text.charAt( i - 1);
+            }
+        }
+
+        return uppercaseLetters;
     }
 
     public String getNonLowercase(String text, int nthChar) {
+        return null;
     }
 
     public String countUppercaseChars(String text, int nthChar) {
+        return null;
     }
 }
